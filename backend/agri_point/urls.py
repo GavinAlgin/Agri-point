3df9143f93d7085014ffe1ea67a589ad82868749
unknown <402308182@my.richfield.ac.za>
2025-08-13 01:33:31 +0200

    Fixing the backend

--- backend/agri_point/urls.py
+++ backend/agri_point/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
-from .views import home
+from .views import register_user, RegisterView, PostListCreateView, CropListCreateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [
-    path('', home, name='home'),
+urlpatterns = [ 
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('posts/', PostListCreateView.as_view(), name='posts'),
+    path('crops/', CropListCreateView.as_view(), name='crops'),
 ]
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from agri_point.views import MyTokenObtainPairView, register_user, protected_route
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', register_user, name='register'),
+    path('protected/', protected_route, name='protected'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('agri_point.urls')),
 ]
